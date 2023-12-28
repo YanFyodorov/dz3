62326a39bfc59e23c77bfe6bdb9dfd1dd1f42694
YanFyodorov <fyodorov.yan@gmail.com>
2023-12-28 13:26:33 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let titleProject = "Оценка стоимости";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 1;
+let percentage = 30;
+let fullPrice = 500;
+let responsive = true;
